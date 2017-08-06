44cad150a9315375349284223dd65e8d03e79a90
ColoMan1 <895843172@qq.com>
2017-08-07 00:06:08 +0800

    乘车详情

---
+++ pages/inquire/bayCarDetails.js
@@ -0,0 +1,70 @@
+// pages/inquire/bayCarDetails.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      topTime:30,
+      bayCarDetails_left: ["上海浦东", "香港", "澳大利亚", "安徽"],
+      bayCarDetails_leftIndex: 0,
+      bayCarDetails_right: ["上海浦东", "香港", "澳大利亚", "安徽"],
+      bayCarDetails_rightIndex: 1
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
